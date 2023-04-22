0904faff792d16a35e16aa27de052e9a492a7a0a
ravi4j <ravi4j@icloud.com>
2023-04-21 21:11:03 -0600

    Refactor Code

--- core/MyBanner.py
+++ core/MyBanner.py
@@ -4,7 +4,7 @@ class MyBanner(Scene):
     def construct(self):
         logo_bk_color = "#2C3E50"
         self.set_camera_bk_color(logo_bk_color);
-        self.banner(logo_bk_color)
+        self.banner(logo_bk_color, '$Welcome\;to\;\mathbb{M}ath$\;tutorial\;!')
         
     def set_camera_bk_color(self, color):
         self.camera.background_color = color
@@ -27,20 +27,26 @@ class MyBanner(Scene):
         triangle1.set_fill(RED, opacity=0.5)
         triangle.move_to(circle.get_center_of_mass())
         triangle1.move_to(circle1.get_center_of_mass())
-        logo_grp = VGroup(top ,circle , circle1 , eye_group)
+        logo_grp = VGroup(top ,circle , circle1, eye_group)
         logo_grp.add_background_rectangle(color=logo_black)
-        return logo_grp    
+        print(logo_grp.submobjects)
+        return logo_grp
 
-    def banner(self,bk_color):
-        sym_grp = self.create_logo(bk_color)
-        ds_m = Tex(r'$Welcome\;to\;\mathbb{M}ath$\;tutorial\;!').scale(3)
+    def create_text(self, tex_string):
+        ds_m = Tex(tex_string).scale(3)
         for letter in ds_m[0]:
-            letter.set_color(random_bright_color())
+            letter.set_color(random_bright_color()) 
+        return ds_m      
+
+    def banner(self,bk_color,banner_text):
+        sym_grp = self.create_logo(bk_color)
+        ds_m = self.create_text(banner_text)
         ds_m.shift(DOWN * 2)
         logo = VGroup(sym_grp, ds_m)   # order matters
         logo.scale(0.5)
         logo.move_to(ORIGIN)
-        self.play(FadeIn(sym_grp),Flash(sym_grp[0]),Flash(sym_grp[1]),Write(ds_m))
+        #self.add(logo)
+        eye_grp=sym_grp.submobjects[4]
+        self.play(FadeIn(sym_grp),Flash(eye_grp[0]),Flash(eye_grp[1]),Write(ds_m))
         self.wait()
-
-   
+        self.add(logo)
