f5466295bff9c8c590df4175819daaa6ada8847f
ravi4j <ravi4j@icloud.com>
2023-04-22 20:25:12 -0600

    Added FractionSession.py Session

--- sessions/FractionSession.py
+++ sessions/FractionSession.py
@@ -1,3 +1,4 @@
+import math
 from manim import *
 import manim
 from manim_voiceover import VoiceoverScene
@@ -11,32 +12,24 @@ class FractionSession(VoiceoverScene):
       # or in this example, record your own voice:
       self.set_speech_service(GTTSService())
       #self.set_speech_service(RecorderService())
-      '''banner = common.MyBanner("$\mathbb{F}$ractions\;in\;5\;minutes\;!") 
+      banner = common.MyBanner("$\mathbb{F}$ractions\;in\;5\;minutes\;!") 
       with self.voiceover(text="Learn fractions in 5 minutes") as tracker:
          self.play(Create(banner))
-         self.wait(2)'''
+         self.wait(2)
        
       fraction_def_grp = self.fraction_def()
       self.add(fraction_def_grp)
-      '''with self.voiceover(text="Fractions represent parts of a whole") as tracker:
-         #self.play(Transform(banner, fraction_def_grp))
+      with self.voiceover(text="Fractions represent parts of a whole") as tracker:
+         self.play(Transform(banner, fraction_def_grp))
          self.add(fraction_def_grp)
-         self.wait()''' 
-
-      '''circle_as_parts = self.circle_as_parts()
-      self.add(circle_as_parts[0]) # add circle 
-      with self.voiceover(text="Half part of the circle") as tracker:
-         self.play(FadeIn(circle_as_parts[1]))
-         circle_as_parts[1].move_to(DOWN * 1.5)
          self.wait()
-      with self.voiceover(text="one forth part of the circle") as tracker:
-         self.play(FadeIn(circle_as_parts[2]))
-         circle_as_parts[2].move_to(LEFT * 1.5 + UP * 1.5 )
-         self.wait()'''
+      
+      for i in [2 , 4 , 6 , 8 , 10]:
+         voice_txt = "Divided circle into" + str(i) + " equal parts"
+         with self.voiceover(text=voice_txt) as tracker:
+            self.create_circle_parts(i)
+            self.wait()
 
-      rect_as_parts = self.rectangle_as_parts()   
-      self.add(rect_as_parts)
-      #self.play(Transform(circle_as_parts ,rect_as_parts))
       self.wait()
 
    def fraction_def(self):  
@@ -49,27 +42,41 @@ class FractionSession(VoiceoverScene):
       fraction_def_grp = VGroup(plane , fraction_def)
       return fraction_def_grp
    
-   def circle_as_parts(self):
-      circle = Circle(color=RED).scale(2)
-      circle_parts = VGroup(circle)
-      for i in range(1,3):
-         label = r"\frac{1}{" + str(2 * i) + "}"
-         randon_color = random_bright_color()
-         cirle_parts_label = MathTex(label , color=randon_color)
-         annularSector = AnnularSector(inner_radius=0,
+   def create_circle_parts(self, parts = 4):
+      PARTS = parts
+      HALF_PARTS = int(PARTS/2)
+      label = r"\frac{1}{" + str( PARTS) + "}"
+      ANGLE = 360/PARTS
+      start_point = (-2,0,0)
+      end_point = (2,0,0)
+      circle = Circle(radius=2,color=RED,fill_opacity=1)
+      circle.set_stroke(color=WHITE , width = 5)
+      start_line = Line(start_point,end_point)
+      copy = start_line.copy()
+      center = Dot()
+      self.play(Create(circle))
+      self.play(Create(center))
+      divider_grp = VGroup()
+      for i in range(HALF_PARTS):
+         divider = copy.rotate(angle=math.radians(ANGLE), about_point=copy.get_center())
+         to = Transform(start_line ,divider)
+         self.play(to)
+         divider_grp.add(divider.copy())
+         self.add(divider.copy()) 
+      label = r"\;=\;\frac{1}{" + str( PARTS) + "}"
+      randon_color = random_bright_color()
+      cirle_parts_label = MathTex(label , color=randon_color , font_size = 144)
+      cirle_parts_label.move_to(RIGHT * 4)
+      self.add(cirle_parts_label) 
+      annularSector = AnnularSector(inner_radius=0,
                          outer_radius=2, 
-                         angle=PI/i, 
+                         angle=PI/HALF_PARTS, 
                          fill_opacity=0.25, 
                          color=randon_color).set_stroke(color=randon_color , width = 5)
-         annularSector.rotate_about_origin(PI/i)
-         cirle_parts_label.move_to(annularSector.get_center())
-         circle_parts.add(VGroup(cirle_parts_label , annularSector))
-      return circle_parts
+      self.add(annularSector) 
+      self.wait()
+      # Remove
+      self.remove(annularSector,cirle_parts_label,divider_grp)
    
-   def rectangle_as_parts(self):
-      rect = Rectangle(color=RED).scale(2)
-      rect_parts = VGroup(rect)
-      
-      return rect_parts
      
     
